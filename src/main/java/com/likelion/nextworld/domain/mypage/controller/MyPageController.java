2019549861f4881caca946c762f0e7654840495a
김나경 <1030n@naver.com>
2025-11-13 21:20:19 +0900

    :sparkles: 북마크 리스트 조회 기능 구현

--- src/main/java/com/likelion/nextworld/domain/mypage/controller/MyPageController.java
+++ src/main/java/com/likelion/nextworld/domain/mypage/controller/MyPageController.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.likelion.nextworld.domain.mypage.dto.ProfileUpdateRequest;
 import com.likelion.nextworld.domain.mypage.service.MyPageService;
 import com.likelion.nextworld.domain.payment.dto.PayItemResponse;
 import com.likelion.nextworld.domain.payment.dto.PointsResponse;
+import com.likelion.nextworld.domain.post.dto.PostResponseDto;
+import com.likelion.nextworld.domain.post.dto.WorkResponseDto;
+import com.likelion.nextworld.domain.scrap.service.ScrapService;
 import com.likelion.nextworld.domain.user.security.UserPrincipal;
 import com.likelion.nextworld.global.response.BaseResponse;
 
@@ -22,6 +29,7 @@ import lombok.RequiredArgsConstructor;
 public class MyPageController {
 
   private final MyPageService myPageService;
+  private final ScrapService scrapService;
 
   @Operation(summary = "내 포인트 조회", description = "사용자의 현재 포인트 정보를 반환합니다.")
   @GetMapping("/points")
@@ -49,5 +57,27 @@ public class MyPageController {
 
     myPageService.updateProfile(user, request);
     return ResponseEntity.ok("프로필이 성공적으로 수정되었습니다.");
+  }
+
+  @Operation(
+      summary = "내 북마크 WORK 리스트 조회",
+      description = "현재 로그인한 사용자가 스크랩한 WORK 목록을 조회합니다. (로그인 필요)")
+  @GetMapping("/scraps/works")
+  public ResponseEntity<BaseResponse<List<WorkResponseDto>>> getMyWorkScraps(
+      @AuthenticationPrincipal UserPrincipal user) {
+
+    List<WorkResponseDto> list = scrapService.getMyWorkScraps(user);
+    return ResponseEntity.ok(BaseResponse.success("WORK 스크랩 목록 조회 완료", list));
+  }
+
+  @Operation(
+      summary = "내 북마크 POST 리스트 조회",
+      description = "현재 로그인한 사용자가 스크랩한 POST 목록을 조회합니다. (로그인 필요)")
+  @GetMapping("/scraps/posts")
+  public ResponseEntity<BaseResponse<List<PostResponseDto>>> getMyPostScraps(
+      @AuthenticationPrincipal UserPrincipal user) {
+
+    List<PostResponseDto> list = scrapService.getMyPostScraps(user);
+    return ResponseEntity.ok(BaseResponse.success("POST 스크랩 목록 조회 완료", list));
   }
 }
